124a9063c76a6a7d5c9b9e5b2ba922428d602a1f
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-08-13 19:37:06 +0200

    Update update2.js

--- elements/netlify_functions/update2.js
+++ elements/netlify_functions/update2.js
@@ -8,20 +8,16 @@ exports.handler = (event, context) => {
 		secret: process.env.FAUNADB_SERVER_SECRET
 	});
 
-	// get counter id and count from url
+	// get counter id and type from url
 	var id = event.path.match(/([^\/]*)\/*$/)[0];
 	var type = event.path.replace("/" + id, "").match(/([^\/]*)\/*$/)[0];
 
 	// get data from db
-	return client.query(q.Get(q.Ref(`classes/${type}/${id}`))).then((value) => {
-		
-		var current_data = JSON.parse(JSON.stringify(value));
-
-		var counter = current_data.data.count;
+	return client.query(q.Get(q.Ref(`classes/${type}/${id}`))).then((current) => {
 
 		// update counter
 		var data = {
-			count: counter + 1,
+			count: JSON.parse(JSON.stringify(current)).data.count + 1,
 			date: new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDay() + "-" + new Date().getHours() + "-" + new Date().getMinutes() + "-" + new Date().getSeconds()
 		}
 		
