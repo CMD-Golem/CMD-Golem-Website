3410588ebf7a547eafa190ced4aa73b56b3ee5b5
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2021-08-15 21:02:49 +0200

    test fauna

--- elements/javascript/special/all_counter.js
+++ elements/javascript/special/all_counter.js
@@ -7,5 +7,18 @@ var readAll = (index_type) => {
 	})
 }
 
-readAll("all_datapacks").then((todos) => { console.log('all todos', todos) });
-readAll("all_resource_packs").then((todos2) => { console.log('all todos', todos2) });
+var datapacks, resource_packs;
+
+
+readAll("all_datapacks").then((response) => {
+	var html = response.map(counter => `
+		<p data-date="${counter.data.date}">${counter.data.name}: ${counter.data.count}</p>
+		`).join('');
+
+	document.getElementsByTagName("main")[0].innerHTML = html;
+
+});
+
+readAll("all_resource_packs").then((response) => { resource_packs = response });
+
+
