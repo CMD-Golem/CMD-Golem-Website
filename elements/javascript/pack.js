de357e7dee0dd9b397b0c9022da2f20aa6a16910
CMD-Golem <64982383+CMD-Golem@users.noreply.github.com>
2022-08-10 22:09:26 +0200

    1.19.2

--- elements/javascript/pack.js
+++ elements/javascript/pack.js
@@ -77,7 +77,7 @@ function openDownload() {
 
 // set newest version
 try {
-	document.getElementById("newest").innerHTML = "1.19.1";
+	document.getElementById("newest").innerHTML = "1.19.2";
 }
 catch (e) {}
 
